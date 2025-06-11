d0ddf0b1d481194cde23342f8a3a1eff77ac49fe
Stone <74167978+s71557@users.noreply.github.com>
2025-06-12 01:02:27 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -9,17 +9,33 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+        echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+
 # 更新 golang 1.24 版本
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
-# 必要的库
-git clone --depth=1 -b main https://github.com/linkease/istore-packages package/istore-packages
-git clone --depth=1 -b master https://github.com/jjm2473/luci-app-diskman package/diskman
-git clone --depth=1 -b dev4 https://github.com/jjm2473/OpenAppFilter package/oaf
-git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
-git clone --depth=1 -b main https://github.com/jjm2473/openwrt-apps package/openwrt-apps
 
 ### 个性化设置
 sed -i 's/iStoreOS/StoneOS/' package/istoreos-files/files/etc/board.d/10_system
@@ -112,6 +128,3 @@ elif [ "$1" = "rk35xx" ]; then
 fi
 
 cat .config
-# 更新Feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
